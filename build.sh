e17b705cf2f29c1bfd110c164908e2c7a645b27b
galaxywk223 <139723111+galaxywk223@users.noreply.github.com>
2025-07-03 18:55:05 +0800

    Build: Move DB migration to start command for runtime execution

--- build.sh
+++ build.sh
@@ -2,9 +2,5 @@
 # Exit on error
 set -o errexit
 
-# 1. 安装所有依赖包
+# 在构建阶段，我们只安装依赖包
 pip install -r requirements.txt
-
-# 2. 使用 Flask-Migrate 的标准命令来运行数据库迁移
-# 这能确保迁移在与您的线上应用完全相同的配置和环境下运行
-flask db upgrade
