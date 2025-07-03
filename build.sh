d31bdb155b16cf9c93a6b2edb47118e5d79c3370
galaxywk223 <139723111+galaxywk223@users.noreply.github.com>
2025-07-03 17:48:54 +0800

    FINAL FIX: Move alembic.ini to root and simplify build script

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
-# 使用 -c 标志明确指定 alembic.ini 的路径
-# 这是解决 "No 'script_location' key found" 问题的最终方法
-alembic -c migrations/alembic.ini upgrade head
+# 使用最标准的 Flask 命令来更新数据库。
+# 它会自动在项目根目录寻找 alembic.ini 文件。
+flask db upgrade
