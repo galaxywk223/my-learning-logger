c424a16c3e8beab5989d08be2b9222846fc390a4
galaxywk223 <139723111+galaxywk223@users.noreply.github.com>
2025-07-21 16:35:33 +0800

    修复部分BUG

--- config.py
+++ config.py
@@ -32,8 +32,17 @@ class DevelopmentConfig(Config):
 
 
 class ProductionConfig(Config):
+    # 从名为 'DATABASE_URL' 的环境变量中获取数据库连接字符串
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(INSTANCE_FOLDER_PATH, 'learning_logs_prod.db')
+
+    # --- 这里是核心修改 ---
+    # 从名为 'UPLOAD_FOLDER_BASE' 的环境变量获取上传根目录
+    # 如果环境变量不存在，则提供一个备用路径
+    UPLOAD_FOLDER_BASE = os.environ.get('UPLOAD_FOLDER_BASE') or '/var/www/learning_logger_uploads'
+    MILESTONE_UPLOADS = os.path.join(UPLOAD_FOLDER_BASE, 'milestones')
+    BACKGROUND_UPLOADS = os.path.join(UPLOAD_FOLDER_BASE, 'backgrounds')
+    # --- 修改结束 ---
 
 
 class TestingConfig(Config):
